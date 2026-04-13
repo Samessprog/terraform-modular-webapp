f024cf7cdc31746eb2cbb739bf5adda2195409aa
Sames <100785560+Samessprog@users.noreply.github.com>
2026-04-13 22:06:24 +0200

    feat: add S3 bucket module with frontend and backend buckets (#8)

---
+++ modules/s3_bucket/variables.tf
@@ -0,0 +1,45 @@
+variable "bucket_name" {
+  type        = string
+  description = "Bucket name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name"
+}
+
+variable "tags" {
+  description = "Tags for bucket"
+  type        = map(string)
+  default     = {}
+}
+
+variable "versioning_enabled" {
+  description = "Enable versioning on the bucket"
+  type        = bool
+  default     = false
+}
+
+variable "restrict_public_buckets" {
+  description = "Restrict public bucket policies"
+  type        = bool
+  default     = true
+}
+
+variable "block_public_acls" {
+  description = "Block public ACLs"
+  type        = bool
+  default     = true
+}
+
+variable "block_public_policy" {
+  description = "Block public bucket policies"
+  type        = bool
+  default     = true
+}
+
+variable "ignore_public_acls" {
+  description = "Ignore public ACLs"
+  type        = bool
+  default     = true
+}
